1f9aac2f87a83579dc6d9a02aa92cd2419eeff27
nebelwerfergranate <acht-acht@mail.ru>
2016-03-06 04:07:51 +0000

    Sorter class implemention

---
+++ Task1/sorter.js
@@ -0,0 +1,36 @@
+function Sorter(data) {
+    this.data = data;
+    this.position = 0;
+    this.isChanged = false;
+    var temp;
+    var i = data.length - 1;
+    var j = 0;
+    this.nextStep = function(){
+        var isDone = false;
+        this.isChanged = false;
+        outer:
+        for ( ; i > 0; ) {
+            if(isDone){
+                if(j -1 === i){
+                    j = 0;
+                }
+                break;
+            }
+            for ( ; j < i; ) {
+                if(isDone){
+                    break outer;
+                }
+                if (data[j] > data[j+1]) {
+                    temp = data[j];
+                    data[j] = data[j+1];
+                    data[j+1] = temp;
+                    this.isChanged = true;
+                }
+                this.position = j;
+                isDone = true;
+                j++;
+            }
+            i--;
+        }
+    }
+}
