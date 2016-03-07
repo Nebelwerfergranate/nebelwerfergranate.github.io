e8173b532f12a5734c4502d6635f5f4ccae2a8ef
nebelwerfergranate <acht-acht@mail.ru>
2016-03-07 20:11:55 +0000

    Soth algorithm changed

--- Task1/sorter.js
+++ Task1/sorter.js
@@ -1,12 +1,43 @@
 function Sorter(data) {
-    this.data = data;
-    this.position = 0;
-    this.isChanged = false;
+    //Fields
     var temp;
     var i = data.length - 1;
     var j = 0;
+    
+    //Properties
+    this.data = data;
+    this.position = 0;
+    this.isChanged = false;
+    this.isFinished = false;
+    
+    //Methods
     this.nextStep = function(){
-        var isDone = false;
+        this.isChanged = false;
+        
+        if(j === i){
+            j = 0;
+            i--;
+            //this.nextStep();
+        }
+        
+        if(i <= 0){
+            this.isFinished = true;
+            return;
+        }
+        
+        if (data[j] > data[j+1]) {
+            temp = data[j];
+            data[j] = data[j+1];
+            data[j+1] = temp;
+            this.isChanged = true;
+        }
+        this.position = j;
+        j++;
+    };
+}       
+        
+        
+        /*var isDone = false;
         this.isChanged = false;
         outer:
         for ( ; i > 0; i--) {
@@ -29,6 +60,4 @@ function Sorter(data) {
                 this.position = j;
                 isDone = true;
             }
-        }
-    }
-}
+        }*/
