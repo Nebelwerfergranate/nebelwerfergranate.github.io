12286268167f8e4155d68a4fd3ec7e1f2f2538d7
nebelwerfergranate <acht-acht@mail.ru>
2016-03-17 20:52:27 +0000

    small css refactoring + big numbers visualization improvement

--- Task2/scripts/main.js
+++ Task2/scripts/main.js
@@ -1,27 +1,42 @@
 document.body.onload = function() {
 	var calc = document.calc;
 	var model = new Calculator();
-	
+
 	calc.addEventListener('click', render);
 	addNumberHandlers();
 	addOperationHandlers();
 	calc.btnClear.addEventListener('click', clear);
 	calc.btnDecimal.addEventListener('click', toDecimal);
-	
-	
-	function render(){
+
+
+	function render() {
 		calc.screen.value = model.screenVal;
+		fitScreenCapacity();
 	}
-	
+
+	function fitScreenCapacity() {
+		var SCREENCAPACITY = 13;
+		var MAXSTRINGLENGTH = 22;
+		if (model.screenVal.length >= SCREENCAPACITY) {
+			calc.screen.classList.add("smallDigits");
+			if (model.screenVal.length >= MAXSTRINGLENGTH) {
+				calc.screen.value = parseFloat(model.screenVal);
+			}
+		}
+		else {
+			calc.screen.classList.remove("smallDigits");
+		}
+	}
+
 	function clear() {
 		model.clear();
 	}
-	
-	function toDecimal(){
+
+	function toDecimal() {
 		model.toDecimal();
 	}
 
-	function addNumberHandlers(){
+	function addNumberHandlers() {
 		calc.btnZero.addEventListener('click', function() {
 			model.numPressed(0);
 		});
@@ -53,7 +68,7 @@ document.body.onload = function() {
 			model.numPressed(9);
 		});
 	}
-	
+
 	function addOperationHandlers() {
 		calc.btnPlus.addEventListener('click', function() {
 			model.doOperation(operationTypes.add);
