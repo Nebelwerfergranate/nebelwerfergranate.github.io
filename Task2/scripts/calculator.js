49121107dadf3035b1e0553bed7739af9a5b6ad4
nebelwerfergranate <acht-acht@mail.ru>
2016-03-17 15:57:51 +0000

    refactoring part 2

---
+++ Task2/scripts/calculator.js
@@ -0,0 +1,68 @@
+function Calculator() {
+    this._result = 0;
+    this._pendingOp = operationTypes.noOperation;
+    this._numberIsNew = true;
+
+    this.screenVal = "0";
+}
+
+Calculator.prototype.numPressed = function(num) {
+    if(!this._numberIsNew){
+        this.screenVal = this.screenVal.concat(num.toString());
+        this._numberIsNew = false;
+        return;
+    }
+    
+    if(num.toString() !== "0"){
+        this._numberIsNew = false;
+    }
+
+    this.screenVal = num.toString();
+};
+
+Calculator.prototype.doOperation = function(op) {
+    if (this._checkOperationIsRequired()) {
+        switch(this._pendingOp){
+            case operationTypes.add:
+                this._result += parseFloat(this.screenVal);
+                break;
+            case operationTypes.subtract:
+                this._result -= parseFloat(this.screenVal);
+                break;
+            case operationTypes.multiply:
+                this._result *= parseFloat(this.screenVal);
+                break;
+            case operationTypes.divide:
+                this._result /= parseFloat(this.screenVal);
+                break;
+            default: 
+                this._result = parseFloat(this.screenVal);
+        }
+        this._pendingOp = op;
+        this._numberIsNew = true;
+    }
+    
+    this.screenVal = this._result.toString();
+}
+
+Calculator.prototype.toDecimal = function() {
+    if (this.screenVal === "0") {
+        this.screenVal = "0.";
+        this._numberIsNew = false;
+    }
+    else if (this.screenVal.indexOf(".") == -1) {
+        this.screenVal += ".";
+        this._numberIsNew = false;
+    }
+};
+
+Calculator.prototype.clear = function() {
+    this.screenVal = "0";
+    this._result = 0;
+    this._pendingOp = operationTypes.noOperation;
+    this._numberIsNew = true;
+};
+
+Calculator.prototype._checkOperationIsRequired = function() {
+    return this._pendingOp === operationTypes.equals || this._numberIsNew === false;
+};
--- Task2/scripts/main.js
+++ Task2/scripts/main.js
@@ -1,137 +1,74 @@
 document.body.onload = function() {
 	var calc = document.calc;
-	var currVal = 0;
-	var numberIsNull = false;
-	var pendingOp = operationTypes.noOperation;
-
-	bindNumberHandlers();
-	bindOperationHandlers();
+	var model = new Calculator();
+	
+	calc.addEventListener('click', render);
+	addNumberHandlers();
+	addOperationHandlers();
 	calc.btnClear.addEventListener('click', clear);
 	calc.btnDecimal.addEventListener('click', toDecimal);
 	
-
-	// обработчик нажатия 
-	// цифровой кнопки
-	function numPressed(num) {
-		if (numberIsNull) {
-			calc.screen.value = num;
-			numberIsNull = false;
-		}
-		else {
-			if (calc.screen.value == "0") {
-				calc.screen.value = num;
-			}
-			else {
-				calc.screen.value += num;
-			}
-		}
+	
+	function render(){
+		calc.screen.value = model.screenVal;
 	}
-
-	// обработчик нажатия
-	// кнопки действия
-	function operation(op) {
-		var screenVal = calc.screen.value;
-		if (numberIsNull && pendingOp !== operationTypes.equals) {
-			calc.screen.value = currVal;
-		}
-		else {
-			numberIsNull = true;
-			if (pendingOp === operationTypes.add) {
-				currVal += parseFloat(screenVal);
-			}
-			else if (pendingOp === operationTypes.subtract) {
-				currVal -= parseFloat(screenVal);
-			}
-			else if (pendingOp === operationTypes.multiply) {
-				currVal *= parseFloat(screenVal);
-			}
-			else if (pendingOp === operationTypes.divide) {
-				currVal /= parseFloat(screenVal);
-			}
-			else {
-				currVal = parseFloat(screenVal);
-			}
-
-			calc.screen.value = currVal;
-			pendingOp = op;
-		}
-	}
-
-	// добавление десятичной точки с числу
-	function toDecimal() {
-		var screenVal = calc.screen.value;
-		if (numberIsNull) {
-			screenVal = "0.";
-			numberIsNull = false;
-		}
-		else {
-			if (screenVal.indexOf(".") == -1)
-				screenVal += ".";
-		}
-		calc.screen.value = screenVal;
-	}
-
-	// Очистка текущего результата
-	function clearEntry() {
-		calc.screen.value = "0";
-		numberIsNull = true;
-	}
-
-	// Полная очистка всех результатов
+	
 	function clear() {
-		currVal = 0;
-		pendingOp = operationTypes.noOperation;
-		clearEntry();
+		model.clear();
+	}
+	
+	function toDecimal(){
+		model.toDecimal();
 	}
 
-	function bindNumberHandlers(){
+	function addNumberHandlers(){
 		calc.btnZero.addEventListener('click', function() {
-			numPressed(0);
+			model.numPressed(0);
 		});
 		calc.btnOne.addEventListener('click', function() {
-			numPressed(1);
+			model.numPressed(1);
 		});
 		calc.btnTwo.addEventListener('click', function() {
-			numPressed(2);
+			model.numPressed(2);
 		});
 		calc.btnThree.addEventListener('click', function() {
-			numPressed(3);
+			model.numPressed(3);
 		});
 		calc.btnFour.addEventListener('click', function() {
-			numPressed(4);
+			model.numPressed(4);
 		});
 		calc.btnFive.addEventListener('click', function() {
-			numPressed(5);
+			model.numPressed(5);
 		});
 		calc.btnSix.addEventListener('click', function() {
-			numPressed(6);
+			model.numPressed(6);
 		});
 		calc.btnSeven.addEventListener('click', function() {
-			numPressed(7);
+			model.numPressed(7);
 		});
 		calc.btnEight.addEventListener('click', function() {
-			numPressed(8);
+			model.numPressed(8);
 		});
 		calc.btnNine.addEventListener('click', function() {
-			numPressed(9);
+			model.numPressed(9);
 		});
 	}
 	
-	function bindOperationHandlers() {
+	function addOperationHandlers() {
 		calc.btnPlus.addEventListener('click', function() {
-			operation(operationTypes.add);
+			model.doOperation(operationTypes.add);
 		});
 		calc.btnMinus.addEventListener('click', function() {
-			operation(operationTypes.subtract);
+			model.doOperation(operationTypes.subtract);
 		});
 		calc.btnMultiply.addEventListener('click', function() {
-			operation(operationTypes.multiply);
+			model.doOperation(operationTypes.multiply);
 		});
 		calc.btnDivide.addEventListener('click', function() {
-			operation(operationTypes.divide);
+			model.doOperation(operationTypes.divide);
 		});
 		calc.btnEquals.addEventListener('click', function() {
-			operation(operationTypes.equals);
+			model.doOperation(operationTypes.equals);
 		});
 	}
 };
