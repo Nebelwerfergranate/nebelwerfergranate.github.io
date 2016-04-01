fb94e3335ca2b101a496e46eb950eeabd2e4b965
nebelwerfergranate <acht-acht@mail.ru>
2016-04-01 04:01:11 +0000

    FreeCellComponent refactoring

--- Task4/scripts/components/freeCellComponent.js
+++ Task4/scripts/components/freeCellComponent.js
@@ -2,27 +2,26 @@ class FreeCellComponent extends Component {
     constructor(options) {
         super(options);
 
-        this._testEl = document.getElementById("test");
-
         // Fields declaration
         this._model = new FreeCellModel();
+        
+        this._view = new FreeCellView({
+            cascades: this._model.cascades,
+            cells: this._model.cells,
+            foundations: this._model.foundations
+        });
+        
         this._draggingElement = null;
-        this._sourceIndo = null;
+        this._sourceInfo = null;
         this._destinationInfo = null;
-        this._cardTemplate = document.getElementById("card-template").innerHTML;
-
+        
 
         // Constructor code 
         this.on("dragstart", this._ondragstartHandler.bind(this));
         this.on("dragover", this._ondragoverHandler.bind(this));
         this.on("drop", this._ondropHandler.bind(this));
-
-        console.dir(this._model._deck);
-        console.dir(this._model._cascades);
-
-        this._renderCascades();
-        this._renderCells();
-        this._renderFoundations();
+        
+        this._view.render();
     }
 
     _ondragstartHandler(event) {
@@ -52,6 +51,8 @@ class FreeCellComponent extends Component {
             cellType: cellType,
             cellNumber: parseInt(cell.getAttribute("data-cell-number")) 
         };
+        
+        this._sourceInfo = info;
         
         if(!this._model.checkSourceIsValid(info)){
             return;
@@ -133,141 +134,5 @@ class FreeCellComponent extends Component {
             type: cell.getAttribute("data-cell-type"),
             number: cell.getAttribute("data-cell-number")
         };
-
-    }
-
-    _setSourceInfoFromEvent(event) {
-        var card = event.target.closest("[data-selector='card']");
-
-        if (card == null) {
-            return;
-        }
-
-        this._sourceIndo = {
-
-        };
-    }
-
-    _renderCascades() {
-        var cascadesBlock = document.querySelector('[data-selector="cascades"]');
-        var cascades = this._model.cascades;
-        var cascadesCount = cascades.length;
-
-        for (let i = 0; i < cascadesCount; i++) {
-            var cards = cascades[i];
-            var cardsCount = cards.length;
-            var selector = '[data-cell-number="' + i + '"]';
-            var cascade = cascadesBlock.querySelector(selector);
-
-            for (let j = 0; j < cardsCount; j++) {
-                var card = cards[j];
-                var cardItem = this._createCardItem(card, j);
-                cascade.innerHTML += cardItem;
-            }
-        }
-    }
-
-    _renderCells() {
-        var cellsBlock = document.querySelector('[data-selector="cells"]');
-        var cells = this._model.cells;
-        var count = cells.length;
-
-        for (let i = 0; i < count; i++) {
-            var selector = '[data-cell-number="' + i + '"]';
-            var cell = cellsBlock.querySelector(selector);
-            var cardItem = this._createCardItem(cells[i]);
-
-            cell.innerHTML = cardItem;
-        }
-    }
-
-    _renderFoundations() {
-        var foundationsBlock = document.querySelector('[data-selector="foundations"]');
-        var foundations = this._model.foundations;
-        var count = foundations.length;
-
-        for (let i = 0; i < count; i++) {
-            var selector = '[data-cell-number="' + i + '"]';
-            var foundation = foundationsBlock.querySelector(selector);
-            var cardItem = this._createCardItem(foundations[i]);
-
-            foundation.innerHTML = cardItem;
-        }
-    }
-
-    _createCardItem(card, position) {
-        let cardItem = _.template(this._cardTemplate)({
-            position: position,
-            cardClass: this._getCardCssClass(card)
-        });
-
-        return cardItem;
-    }
-
-    _getCardCssClass(card) {
-        var className = "";
-
-        switch (card.suit) {
-            case cardsSuits.spades:
-                className += "spades";
-                break;
-            case cardsSuits.hearts:
-                className += "hearts";
-                break;
-            case cardsSuits.diamonds:
-                className += "diamonds";
-                break;
-            case cardsSuits.clubs:
-                className += "clubs";
-                break;
-            default:
-                throw new Error("card suit '" + card.suit + "' doesn't supported");
-        }
-
-        switch (card.rank) {
-            case cardsRanks.two:
-                className += "2";
-                break;
-            case cardsRanks.three:
-                className += "3";
-                break;
-            case cardsRanks.four:
-                className += "4";
-                break;
-            case cardsRanks.five:
-                className += "5";
-                break;
-            case cardsRanks.six:
-                className += "6";
-                break;
-            case cardsRanks.seven:
-                className += "7";
-                break;
-            case cardsRanks.eight:
-                className += "8";
-                break;
-            case cardsRanks.nine:
-                className += "9";
-                break;
-            case cardsRanks.ten:
-                className += "10";
-                break;
-            case cardsRanks.jack:
-                className += "Jack";
-                break;
-            case cardsRanks.queen:
-                className += "Queen";
-                break;
-            case cardsRanks.king:
-                className += "King";
-                break;
-            case cardsRanks.ace:
-                className += "Ace";
-                break;
-            default:
-                throw new Error("card rank '" + card.rank + "' doesn't supported");
-        }
-
-        return className;
     }
 }
